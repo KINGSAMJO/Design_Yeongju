695ab1a0e6a49088e98826b6ee4a33189d5075ce
Julie <yungju9982@naver.com>
2022-04-10 22:30:54 +0900

    commit message

---
+++ app/src/main/java/org/techtown/sopt_30th/semina2/SampleRecyclerView.kt
@@ -0,0 +1,35 @@
+package org.techtown.sopt_30th.semina2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.techtown.sopt_30th.databinding.ActivitySampleRecyclerViewBinding
+
+class SampleRecyclerView : AppCompatActivity() {
+
+    private lateinit var sampleAdapter: SampleAdapter
+    private lateinit var binding : ActivitySampleRecyclerViewBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySampleRecyclerViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAdapter()
+    }
+
+    private fun initAdapter(){
+        sampleAdapter = SampleAdapter()
+        binding.itemMain.adapter = sampleAdapter
+
+        sampleAdapter.userList.addAll(
+            listOf(
+                UserData("이영주", "반갑습니다."),
+                UserData("김세훈", "안녕하세요."),
+                UserData("김아무개", "ㅁㄴㅇㄹ."),
+                UserData("홍길동", "ㅂㅈㄷㄱ."),
+                UserData("나까무라", "ㅋㅌㅊㅍ."),
+                UserData("유병재", "ㅂㅈㄷㄱ.")
+            )
+        )
+        sampleAdapter.notifyDataSetChanged()
+    }
+}
