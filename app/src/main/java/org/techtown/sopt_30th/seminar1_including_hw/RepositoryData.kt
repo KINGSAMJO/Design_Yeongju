210eb6e1fdc1466cf17a63e1d0eec5132f390226
Julie <yungju9982@naver.com>
2022-04-22 22:53:45 +0900

    2차 세미나 과제

---
+++ app/src/main/java/org/techtown/sopt_30th/seminar1_including_hw/RepositoryData.kt
@@ -0,0 +1,6 @@
+package org.techtown.sopt_30th.seminar1_including_hw
+
+data class RepositoryData(
+    val repositoryName : String,
+    val repositoryPart : String
+)
---
+++ app/src/main/java/org/techtown/sopt_30th/seminar2/UserData.kt
@@ -0,0 +1,6 @@
+package org.techtown.sopt_30th.seminar2
+
+data class UserData(
+    val name : String,
+    val introduction: String
+)
