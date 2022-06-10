916a18047ca3c240e5991d4cb470619e7951edb3
이영주 <yungju9982@naver.com>
2022-06-10 23:15:02 +0900

    SignInActivity 수정

--- app/src/main/java/org/techtown/assignment/ui/login/SignInActivity.kt
+++ app/src/main/java/org/techtown/assignment/ui/login/SignInActivity.kt
@@ -1,21 +1,18 @@
 package org.techtown.assignment.ui.login
 
 import android.content.Intent
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import org.techtown.assignment.R
 import org.techtown.assignment.databinding.ActivitySigninBinding
 import org.techtown.assignment.ui.HomeActivity
-import org.techtown.assignment.ui.login.model.RequestSignIn
-import org.techtown.assignment.ui.login.model.ServiceCreator
 import org.techtown.assignment.ui.login.viewmodel.SignInViewModel
 import org.techtown.assignment.util.BaseActivity
-import org.techtown.assignment.util.enqueueUtil
 import org.techtown.assignment.util.showToast
 
-class SignInActivity : BaseActivity<ActivitySigninBinding, SignInViewModel>(R.layout.activity_signin) {
-    private var emptyCheck = false
-    override val viewModel: SignInViewModel = SignInViewModel()
+class SignInActivity :
+    BaseActivity<ActivitySigninBinding, SignInViewModel>(R.layout.activity_signin) {
+    override val viewModel: SignInViewModel by viewModels()
 
     private val resultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -29,40 +26,17 @@ class SignInActivity : BaseActivity<ActivitySigninBinding, SignInViewModel>(R.la
 
     override fun init() {
         binding.viewmodel = viewModel
-        binding.btnLogin.setOnClickListener {
-            val id = binding.etId.text.toString()
-            val pwd = binding.etPwd.text.toString()
-
-            emptyCheck = id.isEmpty() || pwd.isEmpty()
-
-            if (!emptyCheck) {
-                loginNetwork()
-            } else showToast("정보를 모두 기입해주세요.")
-        }
 
         binding.btnJoin.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             resultLauncher.launch(intent)
         }
-    }
 
-    private fun loginNetwork() {
-        val requestSignIn = RequestSignIn(
-            id = binding.etId.text.toString(),
-            password = binding.etPwd.text.toString(),
-        )
-
-        val call = ServiceCreator.soptService.postLogin(requestSignIn)
-
-        call.enqueueUtil(
-            onSuccess = {
-                val data = it?.data
-                showToast("${data?.name}님 반갑습니다!")
+        viewModel.successLogin.observe(this) {
+            if (it) {
+                showToast("${viewModel.id.value}님 안녕하세요.")
                 startActivity(Intent(this@SignInActivity, HomeActivity::class.java))
-            },
-            onError = {
-                showToast("아이디/비밀번호를 확인해주세요.")
-            }
-        )
+            } else showToast("아이디/비밀번호를 확인해주세요.")
+        }
     }
 }
